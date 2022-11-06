63267ac8e0985be229486c696303b4b0a59f08c5
sasha <ssm9960@nyu.edu>
2022-11-06 17:54:06 +0400

    added files

--- Project2/public/app.js
+++ Project2/public/app.js
@@ -6,29 +6,91 @@ socket.on('connect', function() {
 
 window.addEventListener('load', ()=>{
 
-    let btn = document.querySelector(".btn");
-    console.log(btn);
 
-    if(btn != null)
-    {
+    let courseName = "";
+
+    let btn = document.querySelector(".btn");
+
         btn.addEventListener("click",()=>{
+
+            removeMessages();
 
             let textVal = document.getElementById("textVal")
             let courseWindow = document.querySelector('.courseContainer');
             courseWindow.style.display = "none";
-            //console.log(textVal.value);
 
-            let commentObj = {
+            let eachContainer = document.querySelector('.eachContainer');
+            eachContainer.style.display = "flex";
+
+            courseName = textVal.value;
+
+            let courseHeading = document.querySelector('.courseHeading');
+            courseHeading.innerHTML = courseName;
+    
+            let url = "/comments?selectedCourse="+courseName;
+
+            fetch(url)
+            .then(res => res.json())
+            .then(data =>{
+
+                let arr = data.comments;
+                arr.forEach(e => {
+                    //console.log(e.comment);  
+                    addMessage(e.comment);    
+                });
+
+            })
+
+            /*let commentObj = {
             "msg" : textVal.value,
             "updateAt" : new Date()
             };
 
             let commentObjJSON = JSON.stringify(commentObj);
-            socket.emit('data',commentObjJSON);
+            socket.emit('data',commentObjJSON);*/
 
         });
-    }
+    
+
+    let button = document.querySelector('.btnComment');
+    button.addEventListener('click',()=>{
+
+        let text = document.getElementById("textValComment");
+        addMessage(text.value);
+
+        let commentObj = {
+            "courseName" : courseName,
+            "comment" : text.value,
+            "updateAt" : new Date()
+            };
+
+            let commentObjJSON = JSON.stringify(commentObj);
+            socket.emit('data',commentObj);
+
+    });
   
 
 });
  
+function addMessage( message){
+
+    let elem = document.createElement('div');
+        elem.innerHTML = message;
+        elem.classList.add('comment');
+
+        let container = document.querySelector('.commentContainer');
+        container.appendChild(elem);
+}
+
+function removeMessages(){
+
+    let container = document.querySelector('.commentContainer');
+    let child = container.lastElementChild;
+
+    while(child)
+    {
+        container.removeChild(child);
+        child = container.lastElementChild;
+    }
+
+}
--- Project2/server.js
+++ Project2/server.js
@@ -8,6 +8,7 @@ db.loadDatabase();
 app.use('/', express.static('public'));
 
 
+
 let http = require("http");
 let server = http.createServer(app);
 
@@ -20,12 +21,31 @@ let io = require("socket.io");
 io = new io.Server(server);
 
 
+app.get('/comments', (req,res)=>{
+    
+    let c = req.query.selectedCourse;
+
+    db.find({courseName : c}).sort({ updateAt: 1 }).exec(function (err, docs) {
+        if(err) {
+            res.json({task: "task failed"})
+        } else {
+            let obj = {comments: docs};
+            res.json(obj);
+        }
+      });
+
+});
+
+
 io.sockets.on('connection', function(socket) {
     console.log("We have a new client: " + socket.id);
     
     socket.on('data', (data)=>{
+
         console.log(data);
-        db.insert(data);
+        db.insert(data, (err, newDoc)=>{
+            //console.log(newDoc);
+        });
 
         io.sockets.emit('sdata',data);
     })
@@ -34,5 +54,5 @@ io.sockets.on('connection', function(socket) {
     socket.on('disconnect', function() {
         console.log("A client has disconnected: " + socket.id);
     });
-    
+
 });
