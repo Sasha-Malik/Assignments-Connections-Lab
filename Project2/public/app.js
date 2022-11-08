22741dac252ea8f48d225c7a7c96c02f81ca844c
Avinash Gyavali <avinash@Avinashs-MacBook-Air.local>
2022-11-08 13:09:47 +0400

    Chart js working full time

--- Project2/public/app.js
+++ Project2/public/app.js
@@ -1,17 +1,72 @@
 let socket = io();
 
+const labels = [
+    'Workload',
+    'Grading',
+    'Exams',
+    'Content',
+    'Professor',
+  ];
+
+const chartdata = {
+labels: labels,
+datasets: [{
+  label: 'Very Easy',
+  data: [0, 0, 0, 0, 0],
+  backgroundColor: 
+  [
+    'rgba(255, 99, 132, 0.2)',
+    'rgba(255, 159, 64, 0.2)',
+    'rgba(255, 205, 86, 0.2)',
+    'rgba(75, 192, 192, 0.2)',
+    'rgba(54, 162, 235, 0.2)',
+  ],
+  borderColor: [
+    'rgb(255, 99, 132)',
+    'rgb(255, 159, 64)',
+    'rgb(255, 205, 86)',
+    'rgb(75, 192, 192)',
+    'rgb(54, 162, 235)',
+  ],
+  borderWidth: 1
+}]
+};
+
+
+
+const config = {
+    type: 'bar',
+    data: chartdata,
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    },
+  };
+
 socket.on('connect', function() {
     console.log("Connected");
   });
 
-window.addEventListener('load', ()=>{
 
+let myChart;
 
+window.addEventListener('load', ()=> {
+ 
+
+    let chart = document.getElementById("myChart").getContext("2d");
+    myChart = new Chart(
+        chart,
+        config
+    );
     let courseName = "";
 
     socket.on( 'sdata' , (data)=> {
         addMessage(data.comment);
     });
+
 
 
     fetch('/courses')
@@ -31,6 +86,7 @@ window.addEventListener('load', ()=>{
 
                 courseName = child.innerHTML;
                 showComments(courseName);
+                showPoll(courseName);
 
             });
 
@@ -55,9 +111,8 @@ window.addEventListener('load', ()=>{
 
             let textVal = document.getElementById("textVal")
             courseName = textVal.value;
-
             showComments(courseName);
-
+            showPoll(courseName);
         });
     
 
@@ -75,9 +130,8 @@ window.addEventListener('load', ()=>{
                 };
               
                 socket.emit('data',commentObj);
-
             }
-           
+
     });
 
 
@@ -98,8 +152,8 @@ window.addEventListener('load', ()=>{
 
         let pollobjJSON = JSON.stringify(pollObj);
         socket.emit('poll',pollObj);
-
         console.log(pollobjJSON);
+        showPoll(courseName);
     })
   
 
@@ -141,67 +195,77 @@ function showComments(courseName){
 
             let courseHeading = document.querySelector('.courseHeading');
             courseHeading.innerHTML = courseName;
-
     
             let url = "/comments?selectedCourse="+courseName;
-            
 
             fetch(url)
             .then(res => res.json())
             .then(data =>{
 
                 let arr = data.comments;
-                //console.log(arr);
                 arr.forEach(e => {
                     addMessage(e.comment);    
                 });
 
             })
-
-
-            let url2 = "/polls?selectedCourse="+courseName;
-            fetch(url2)
-            .then(res => res.json())
-            .then(data =>{
-
-                
-                let arr = data.poll; // Gets all the poll for the courses
-                let t_wpoll = 0;     // Total Workload
-                let t_gpoll = 0;     // Total Grading
-                let t_epoll = 0;     // Total Exams
-                let t_cpoll = 0;     // Total Content
-                let t_ppoll = 0;     // Total professor     
-                let totaluserpoll = 0;
-                
-
-                let a_wpoll = 0;     // Average Workload
-                let a_gpoll = 0;     // Average Grading
-                let a_epoll = 0;     // Average Exams
-                let a_cpoll = 0;     // Average Content
-                let a_ppoll = 0;     // Average Professor
-
-                //console.log(arr);
-
-                arr.forEach(e => {
-                   t_wpoll += parseInt(e.polldata[0]);
-                   t_gpoll += parseInt(e.polldata[1]);
-                   t_epoll += parseInt(e.polldata[2]);
-                   t_cpoll += parseInt(e.polldata[3]);
-                   t_ppoll += parseInt(e.polldata[4]);
-                   totaluserpoll++;
-                });
-
-                a_wpoll = t_wpoll/totaluserpoll;
-                a_gpoll = t_gpoll/totaluserpoll;
-                a_epoll = t_epoll/totaluserpoll;
-                a_cpoll = t_cpoll/totaluserpoll;
-                a_ppoll = t_ppoll/totaluserpoll;
-
-                /*console.log("Average Workload :" +a_wpoll);
-                console.log("Average Grading :" +a_gpoll);
-                console.log("Average Exams:" +a_epoll);
-                console.log("Average Content :" +a_cpoll);
-                console.log("Average Professor :" +a_ppoll);*/
-
-            })
 }
+
+function showPoll(courseName)
+{
+
+    let url2 = "/polls?selectedCourse="+courseName;
+    fetch(url2)
+    .then(res => res.json())
+    .then(data =>{
+
+        
+        let pollsarray = data.poll; // Gets all the poll for the courses
+        let t_wpoll = 0;     // Total Workload
+        let t_gpoll = 0;     // Total Grading
+        let t_epoll = 0;     // Total Exams
+        let t_cpoll = 0;     // Total Content
+        let t_ppoll = 0;     // Total professor     
+        let totaluserpoll = 0;
+        
+        console.log(pollsarray);
+
+        let a_wpoll = 0;     // Average Workload
+        let a_gpoll = 0;     // Average Grading
+        let a_epoll = 0;     // Average Exams
+        let a_cpoll = 0;     // Average Content
+        let a_ppoll = 0;     // Average Professor
+
+      
+
+        pollsarray.forEach(e => {
+           t_wpoll += parseInt(e.polldata[0]);
+           t_gpoll += parseInt(e.polldata[1]);
+           t_epoll += parseInt(e.polldata[2]);
+           t_cpoll += parseInt(e.polldata[3]);
+           t_ppoll += parseInt(e.polldata[4]);
+           totaluserpoll++;
+        });
+
+        a_wpoll = t_wpoll/totaluserpoll;
+        a_gpoll = t_gpoll/totaluserpoll;
+        a_epoll = t_epoll/totaluserpoll;
+        a_cpoll = t_cpoll/totaluserpoll;
+        a_ppoll = t_ppoll/totaluserpoll;
+
+        console.log("Average Workload :" +a_wpoll);
+        console.log("Average Grading :" +a_gpoll);
+        console.log("Average Exams:" +a_epoll);
+        console.log("Average Content :" +a_cpoll);
+        console.log("Average Professor :" +a_ppoll);
+
+
+        average_data = [a_wpoll,a_gpoll,a_epoll,a_cpoll,a_ppoll];
+        chartdata.datasets[0].data = average_data;
+
+        myChart.update();
+
+
+        console.log(chartdata.datasets[0].data);
+    })
+}
+
