2925dc337d4ba994163ef208476536950b27b776
sasha <ssm9960@nyu.edu>
2022-11-22 00:38:04 +0400

    .

---
+++ Assignment5 (Library)/app.js
@@ -0,0 +1,19 @@
+
+var c1 = document.querySelector('#c1');
+
+c1.addEventListener('click', function (e) {
+    window.location.href = "https://course-search.glitch.me/";
+  });
+
+  var c2 = document.querySelector('#c2');
+
+c2.addEventListener('click', function (e) {
+    window.location.href = "https://sasha-malik.github.io/Assignments-Connections-Lab/Assignment4/";
+  });
+
+
+var c3 = document.querySelector('#c3');
+
+c3.addEventListener('click', function (e) {
+    window.location.href = "https://etch-a-sketch-sasha.glitch.me/";
+  });
